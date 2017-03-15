90e921a40ff1ec454f6ee4a29c5dc5efb99e348e
The Bart <marvetickxiii@gmail.com>
2017-03-15 16:17:15 +0100

    done

---
+++ CLIENT/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientCON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartClient();
+        }
+        public static void StartClient()
+        {
+            byte[] bytes = new byte[1024];
+            try
+            {
+                IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+                IPAddress ipAddress = IPAddress.Parse("192.168.2.246");
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 10000);
+                Socket sender = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Stream, ProtocolType.Tcp);
+                try
+                {
+                    sender.Connect(remoteEP);
+
+                    Console.WriteLine("Socket connected to {0}",
+                        sender.RemoteEndPoint.ToString());
+                    Console.WriteLine("Write a message");
+                    string message = Console.ReadLine();
+                    // Encode the data string into a byte array.
+                    byte[] msg = Encoding.ASCII.GetBytes(message + "<HashHash>");
+
+                    // Send the data through the socket.
+                    int bytesSent = sender.Send(msg);
+
+                    // Receive the response from the remote device.
+                    int bytesRec = sender.Receive(bytes);
+                    Console.WriteLine("Echoed test = {0}",
+                        Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                    // Release the socket.
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
